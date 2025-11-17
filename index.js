257d698e767ae2a99db86e136e703a9b57b2ae60
mohanad <mohanad.mkhz@gmail.com>
2025-11-17 03:48:14 +0200

    add app url

--- index.js
+++ index.js
@@ -110,7 +110,9 @@ app.get("/api/league-priorities", (req, res) => {
 });
 
 app.get("/api/download-url", (req, res) => {
-  res.json({ url: "https://app.com" });
+  res.json({
+    url: "https://expo.dev/accounts/mohanad_kz/projects/matchora/builds/82a40c2f-5364-4dfc-bbe2-029c17e8a76f",
+  });
 });
 
 // Start the server
