fc4c5450bf53ec7c0fc4dc39a99b201117419ca4
mohanad <mohanad.mkhz@gmail.com>
2025-11-17 03:40:02 +0200

    add .env file

--- index.js
+++ index.js
@@ -1,82 +1,88 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
 
 // Define the API route
-app.get('/api/matches/today', async (req, res) => {
+app.get("/api/matches/today", async (req, res) => {
   try {
-    const response = await axios.get('https://www.yalla-shoot-365.com/matches/npm/');
+    const response = await axios.get(`${process.env.API}/matches/npm/`);
     // Send the entire data object as JSON
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching matches:', error);
-    res.status(500).json({ error: 'Failed to fetch matches' });
+    console.error("Error fetching matches:", error);
+    res.status(500).json({ error: "Failed to fetch matches" });
   }
 });
 
 // Define the new API route for fetching matches by date
-app.get('/api/matches/date/:date', async (req, res) => {
+app.get("/api/matches/date/:date", async (req, res) => {
   try {
     const { date } = req.params;
-    const apiUrl = `https://www.yalla-shoot-365.com/matches/npm/?date=${date}&lang=27&time=+02:00`;
+    const apiUrl = `${process.env.API}/matches/npm/?date=${date}&lang=27&time=+02:00`;
     const response = await axios.get(apiUrl);
     // Send the STING-WEB-Matches array as JSON
-    res.json(response.data['STING-WEB-Matches']);
+    res.json(response.data["STING-WEB-Matches"]);
   } catch (error) {
     console.error(`Error fetching matches for date ${req.params.date}:`, error);
-    res.status(500).json({ error: 'Failed to fetch matches for the specified date' });
+    res
+      .status(500)
+      .json({ error: "Failed to fetch matches for the specified date" });
   }
 });
 
 // Define the new complex API route for fetching match details and stats
-app.get('/api/match/:matchId', async (req, res) => {
+app.get("/api/match/:matchId", async (req, res) => {
   try {
     const { matchId } = req.params;
 
-    const eventsUrl = `https://www.yalla-shoot-365.com/matches/npm/events/?MatchID=${matchId}&lang=27&time=+02:00`;
-    const statsUrl = `https://www.yalla-shoot-365.com/matches/npm/stats/?MatchID=${matchId}`;
+    const eventsUrl = `${process.env.API}/matches/npm/events/?MatchID=${matchId}&lang=27&time=+02:00`;
+    const statsUrl = `${process.env.API}/matches/npm/stats/?MatchID=${matchId}`;
 
     // Use Promise.all to fetch from both URLs concurrently
     const [eventsResponse, statsResponse] = await Promise.all([
       axios.get(eventsUrl),
-      axios.get(statsUrl)
+      axios.get(statsUrl),
     ]);
 
     // Combine the results into a single JSON object
     const combinedResponse = {
       details: eventsResponse.data,
-      stats: statsResponse.data
+      stats: statsResponse.data,
     };
 
     res.json(combinedResponse);
   } catch (error) {
-    console.error(`Error fetching details for match ${req.params.matchId}:`, error);
-    res.status(500).json({ error: 'Failed to fetch match details' });
+    console.error(
+      `Error fetching details for match ${req.params.matchId}:`,
+      error
+    );
+    res.status(500).json({ error: "Failed to fetch match details" });
   }
 });
 
-app.get('/api/image', async (req, res) => {
+app.get("/api/image", async (req, res) => {
   try {
     const imagePath = req.query.path;
     if (!imagePath) {
-      return res.status(400).send('Image path is required');
+      return res.status(400).send("Image path is required");
     }
-    const imageUrl = `https://www.yalla-shoot-365.com${imagePath}`;
+    const imageUrl = `${process.env.API}${imagePath}`;
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: imageUrl,
-      responseType: 'stream',
+      responseType: "stream",
     });
     response.data.pipe(res);
   } catch (error) {
-    console.error('Error fetching image:', error);
-    res.status(500).send('Failed to fetch image');
+    console.error("Error fetching image:", error);
+    res.status(500).send("Failed to fetch image");
   }
 });
 
@@ -99,12 +105,12 @@ const LEAGUE_PRIORITIES = {
   5421: { priority: 95 },
 };
 
-app.get('/api/league-priorities', (req, res) => {
+app.get("/api/league-priorities", (req, res) => {
   res.json(LEAGUE_PRIORITIES);
 });
 
-app.get('/api/download-url', (req, res) => {
-  res.json({ url: 'https://app.com' });
+app.get("/api/download-url", (req, res) => {
+  res.json({ url: "https://app.com" });
 });
 
 // Start the server
